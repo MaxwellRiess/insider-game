09ef8413254e77acf346d539f6f21611899a108b
Maxwell Riess <maxwell@maxwellriess.com>
2025-12-01 00:52:10 +0000

    Prepare for Render deployment

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,8 +1,7 @@
 import { io } from 'socket.io-client';
 
-// For local development, assume server is on port 3001
-// In production, this would be the same origin or configured env var
-const URL = 'http://localhost:3001';
+// In production, undefined lets socket.io-client connect to the same origin
+const URL = import.meta.env.PROD ? undefined : 'http://localhost:3001';
 
 export const socket = io(URL, {
     autoConnect: true
--- server/index.js
+++ server/index.js
@@ -16,9 +16,20 @@ const io = new Server(server, {
 
 const PORT = process.env.PORT || 3001;
 
+const path = require('path');
+
 // Basic health check
-app.get('/', (req, res) => {
+app.get('/health', (req, res) => {
     res.send('Digital Insider Server is running');
+});
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 const GameManager = require('./gameManager');
